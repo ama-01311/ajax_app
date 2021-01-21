1dbe3b17fdd4ed305bb6d408e6144f50f5e2905b
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-01-21 11:30:22 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
+  # get 'posts/new', to: 'posts#new' 必要なし
   post 'posts', to: 'posts#create'
 end
